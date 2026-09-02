2d2fa24b3e8e9551bb65f619e8c932e72591668c
Ankit soni <asoni.soni12@gmail.com>
2026-09-02 16:41:13 +0530

    Add buttons on Home

--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <main className="w-full">
           <section className="grid grid-cols-2 h-[80vh] w-full">
-            <div className="text flex items-center justify-center">
+            <div className="text flex flex-col gap-5 items-center justify-center">
               <p className={`text-4xl font-poppin font-bold`}>Best Url Shortner in The Market.</p>
+              <div className="buttons flex items-center gap-4">
+                <Link href={"/shorten"} className="bg-blue-400 hover:bg-blue-300 text-lg font-semibold px-4 py-1 rounded-md text-black">Try Now</Link>
+                <Link href={"https://github.com/Ankryptic/jiri-link"} target="_blank" className="bg-blue-400 hover:bg-blue-300 text-lg font-semibold px-4 py-1 rounded-md text-black">Github</Link>
+              </div>
             </div>
             <div className="cat relative w-full">
               <Image
