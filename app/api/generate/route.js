29968cad88b2a24952f3721a83b45d2796a1e6ab
Ankit soni <asoni.soni12@gmail.com>
2026-09-04 18:17:01 +0530

    integerate mongodb

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,7 +1,38 @@
 import { NextResponse } from "next/server";
+import clientPromise from "@/lib/mongodb";
 
-export function POST(request){
-    return NextResponse.json({
-        success: true, error: false, message: "ShortURL created successfully"
-    })
+export async function POST(request) {
+    try {
+        const req = await request.json()
+
+        // connecting to database
+        let client = await clientPromise
+        let db = client.db("jirilink")
+        let collection = db.collection("shortURL")
+
+        // Data
+        let data = {
+            url: req.url,
+            shorturl: req.shorturl
+        }
+
+        // Adding data to database
+       await collection.insertOne( data )
+        
+        return NextResponse.json({
+            success: true, 
+            error: false, 
+            message: "ShortURL created successfully"
+        })
+
+    } catch (error) {
+        console.log(error)
+
+        return NextResponse.json({
+            success: false, 
+            error: true, 
+            Message: "Error Creating ShortURL"
+        })
+    }
+
 }
