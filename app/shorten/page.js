c9c2b336364520d1b121951c7f06816ea4abbd40
Ankit soni <asoni.soni12@gmail.com>
2026-09-05 15:31:28 +0530

    add empty field validation

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -4,8 +4,37 @@ import { useState } from "react";
 import Link from "next/link";
 
 const Shorten = () => {
+    const [urlForm, setUrlForm] = useState({
+        url: "",
+        prefferedUrl: ""
+    })
+    const [error, setError] = useState({
+        urlError: "",
+        prefferedError: ""
+    })
     const [generatedURL, setGeneratedURL] = useState("")
 
+    const handleGenerate = () => {
+        let res = validateForm()
+
+        if(!res) {
+            return
+        }
+        console.log(urlForm)
+    }
+
+    const validateForm = () => {
+        if(urlForm.url === "" || urlForm.prefferedUrl === ""){
+            if(urlForm.url === ""){
+                setError(prev => ({...prev, urlError: "Field is Required!"}))
+            }
+            if(urlForm.prefferedUrl === ""){
+                setError(prev => ({...prev, prefferedError: "Field is Required!"}))
+            }
+
+            return false;
+        }
+    }
 
     return (
         <div className="flex flex-1 flex-col">
@@ -18,14 +47,31 @@ const Shorten = () => {
 
                     </div>
                     <div className="w-full h-[60%] flex flex-col items-center justify-center gap-4">
-                        <input type="text" placeholder="Enter URL Here"
-                            className="w-[50%] bg-gray-900 border-2 border-blue-400 rounded-md font-popp px-2 py-px"
-                        />
-                        <input type="text" placeholder="Enter your preffered short URL text"
-                            className="w-[50%] bg-gray-900 border-2 border-blue-400 rounded-md font-popp px-2 py-px"
-                        />
+                        <div className="w-full flex flex-col items-center">
+                            <input type="text" placeholder="Enter URL Here"
+                                className={`w-[50%] bg-gray-900 border-2 border-blue-400 rounded-md font-popp px-2 py-px
+                                    ${error.urlError ? "border-red-400" : "border-blue-400"}
+                                    `}
+                                value={urlForm.url}
+                                onChange={(e) => setUrlForm({ ...urlForm, url: e.target.value })}
+                            />
+                            {error.urlError && <span className="w-[50%] text-sm text-red-400 p-1">{error.urlError}</span>}
+                        </div>
+
+                        <div className="w-full flex flex-col items-center">
+                            <input type="text" placeholder="Enter your preffered short URL text"
+                                className={`w-[50%] bg-gray-900 border-2 border-blue-400 rounded-md font-popp px-2 py-px
+                                    ${error.urlError ? "border-red-400" : "border-blue-400"}
+                                    `}
+                                value={urlForm.prefferedUrl}
+                                onChange={(e) => setUrlForm({ ...urlForm, prefferedUrl: e.target.value })}
+                            />
+                            {error.prefferedError && <span className="w-[50%] text-sm text-red-400 p-1">{error.prefferedError}</span>}
+                        </div>
+
+
                         <div className="generate w-full flex items-center justify-center">
-                            <button className="w-[50%] bg-blue-400 hover:bg-blue-300 px-4 py-1 rounded-lg text-black cursor-pointer font-bold">Generate</button>
+                            <button className="w-[50%] bg-blue-400 hover:bg-blue-300 px-4 py-1 rounded-lg text-black cursor-pointer font-bold" onClick={handleGenerate}>Generate</button>
                         </div>
 
                         <div className="w-full flex flex-col items-center justify-center">
@@ -47,8 +93,8 @@ const Shorten = () => {
                                 <li className="list-none text-center">
                                     <span className="text-blue-400 font-bold">-- Start -- </span>
                                 </li>
-                               <li className="text-sm">
-                                <Link href={"https://localhost:3000/google"}>https://localhost:3000/google</Link>
+                                <li className="text-sm">
+                                    <Link href={"https://localhost:3000/google"}>https://localhost:3000/google</Link>
                                 </li>
                                 <li className="list-none text-center">
                                     <span className="text-red-400 font-bold">-- End -- </span>
